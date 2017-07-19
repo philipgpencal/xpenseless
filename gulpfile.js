668a8933d4abeca4dcbda8f1ef6383ad2d9f658f
Pedro Banali <pedro_banali@hotmail.com>
2017-07-18 21:05:27 -0300

    First Commit

--- gulpfile.js
+++ gulpfile.js
@@ -86,10 +86,11 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+// gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+gulp.task('dev', ['browserSync', 'less'], function() {
     gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
+    // gulp.watch('css/*.css', ['minify-css']);
+    // gulp.watch('js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
